fa94ce0c30d90f942698c9de72ad6130ea2b7097
Jerry Cimo <jcimo1@binghamton.edu>
2015-12-03 15:57:47 -0500

    function skeleton

--- boyer-moore.h
+++ boyer-moore.h
@@ -1,1 +1,6 @@
 // BOYER MOORE STRING SEARCH ALGORITHM
+
+int boyer_moore_search(string base_str, string key_str) {
+    
+    return -1;
+}
--- knuth-morris-pratt.h
+++ knuth-morris-pratt.h
@@ -1,1 +1,6 @@
 // KNUTH MORRIS PRATT STRING SEARCH ALGORITHM
+
+int knuth_morris_pratt_search(string base_str, string key_str) {
+    
+    return -1;
+}
