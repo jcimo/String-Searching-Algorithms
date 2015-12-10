ffe0585fa2615e5a764e546b712e0bfc9abe0758
mroefs1 <mroefs1@binghamton.edu>
2015-12-09 19:24:27 -0500

    bm change

--- boyer-moore.h
+++ boyer-moore.h
@@ -38,59 +38,38 @@ void fillBadChar(string key, int keyLength, int badCharTable[NUM_CHARS]){
 
 vector<int> boyer_moore_search(string key, string base) {
     vector<int> finds; //indexes where key is found 
-    // string base, key, line; 
-    // //Dealing with input/output below 
-    // ifstream input(inputfile.c_str());
-    // int lineCount = 0; 
-    // if(input.is_open()){
-    // 	while(getline(input,line)){
-    // 		if(lineCount == 0){
-    // 			key = line;
-    // 			lineCount++;
-    // 		}else{
-    // 			base = base + line; 
-    // 		}
-    // 	}
+	int baseLength = base.length();
+	int keyLength = key.length(); 
+	int badCharTable[NUM_CHARS]; 
+	cout << "Key: " << key << endl; 
+	cout << "Base: " << base << endl;
+	int shiftIndex = 0; //location of the match
 
-	//Time to actually do the search 
+	//fill the badCharTable 
+	fillBadChar(key, keyLength, badCharTable);
 
-    	int baseLength = base.length();
-    	int keyLength = key.length(); 
-    	int badCharTable[NUM_CHARS]; 
-    	cout << "Key: " << key << endl; 
-    	cout << "Base: " << base << endl;
-    	int shiftIndex = 0; //location of the match
+	while(shiftIndex <= (baseLength - keyLength)){
+		cout << "shiftIndex: " << shiftIndex << endl;
+		int keyIndex = keyLength - 1; //start at the end 
+		//keep searching backwards, checking that the characters line up 
+		while(keyIndex >= 0 && key[keyIndex] == base[keyIndex + shiftIndex]){
+			keyIndex--; 
+		}
+			
+		//if the pattern is found, then keyIndex should be -1. add it to the vector
+		if(keyIndex < 0){
+			cout << "Got it! " << " shiftIndex: " << shiftIndex << " keyIndex: " << keyIndex << endl;
+				finds.push_back(shiftIndex+1);
 
-    	//fill the badCharTable 
-    	fillBadChar(key, keyLength, badCharTable);
-
-    	while(shiftIndex <= (baseLength - keyLength)){
-    		cout << "shiftIndex: " << shiftIndex << endl;
-    		int keyIndex = keyLength - 1; //start at the end 
-    		//keep searching backwards, checking that the characters line up 
-    		while(keyIndex >= 0 && key[keyIndex] == base[keyIndex + shiftIndex]){
-    			keyIndex--; 
-    		}
-    			
-			//if the pattern is found, then keyIndex should be -1. add it to the vector
-			if(keyIndex < 0){
-				cout << "Got it! " << " shiftIndex: " << shiftIndex << " keyIndex: " << keyIndex << endl;
-  				finds.push_back(shiftIndex+1);
-
-				//shift to line up the next char in base lines up with the last occurence in the key
-				//must check to see if we found the pattern at the end of the text 
-				shiftIndex += (shiftIndex + keyLength < baseLength) ? keyLength - badCharTable[base[shiftIndex+keyLength]] : 1;  				
-			}else {
-				//shift so the bad character (not matched) aligns with the last occurence in the key
-				//max is used to ensure we shift in the right direction
-				shiftIndex += max(1, keyIndex - badCharTable[base[shiftIndex + keyIndex]]); 
-			}	
-    	}
-
-    	input.close(); 
-    }else{
-    	cout << "Could not open inputfile " + inputfile + ". Returning empty vector." << endl; 
-    }
+			//shift to line up the next char in base lines up with the last occurence in the key
+			//must check to see if we found the pattern at the end of the text 
+			shiftIndex += (shiftIndex + keyLength < baseLength) ? keyLength - badCharTable[base[shiftIndex+keyLength]] : 1;  				
+		}else {
+			//shift so the bad character (not matched) aligns with the last occurence in the key
+			//max is used to ensure we shift in the right direction
+			shiftIndex += max(1, keyIndex - badCharTable[base[shiftIndex + keyIndex]]); 
+		}	
+	}
 
     return finds;
 }
